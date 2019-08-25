6b33456b3703c2db9e503d781be18da8e7133efd
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-08-24 20:43:39 -0700

    Handle 404

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile('/index.html'))
 
+app.use(function(req, res) {
+    res.type('text/plain')
+    res.status(404)
+    res.send('404')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
