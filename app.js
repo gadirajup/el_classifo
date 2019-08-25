29a58162248a976ccf203e05b5a0772f308c2a1f
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-08-25 10:35:04 -0700

    Upload with Multer

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express')
+const multer = require('multer')
+const ejs = require('ejs')
+const path = require('path')
+
 const app = express()
 const port = 3000
 
-app.use(express.static('public'));
+const storage = multer.diskStorage({
+    destination: './public/uploads',
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+})
 
-app.get('/', (req, res) => res.sendFile('/index.html'))
+const upload = multer({
+    storage: storage
+}).single('myImage');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => res.render('index'))
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if(err){
+            res.render('index', {
+                msg: err
+            })
+        } else {
+            console.log(req.file);
+            res.send('Test')
+        }
+    })
+})
 
 app.use(function(req, res) {
     res.type('text/plain')
@@ -12,4 +42,4 @@ app.use(function(req, res) {
     res.send('404')
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
